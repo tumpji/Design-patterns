adbee41990b522acb35dc0c64ed189606e51e240
Jiří Tumpach <tumpji@gmail.com>
2017-07-22 00:08:44 +0200

    change in licence header

--- creational/singleton/singleton_mt.cpp
+++ creational/singleton/singleton_mt.cpp
@@ -1,17 +1,17 @@
-//This file is part of Foobar.
+//This file is part of Thread-safe-design-patterns.
 //
-//    Foobar is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
+//    Thread-safe-design-patterns is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    Foobar is distributed in the hope that it will be useful,
+//    Thread-safe-design-patterns is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+//    GNU Lesser General Public License for more details.
 //
-//    You should have received a copy of the GNU General Public License
-//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Thread-safe-design-patterns.  If not, see <http://www.gnu.org/licenses/>.
 
 // It behaves like non-thread safe variant.
 // Only difference is that threre is 
