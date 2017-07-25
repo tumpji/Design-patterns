e3244aa96c973ffaaf04e25cd2bc4a746dfabf7a
Jiří Tumpach <tumpji@gmail.com>
2017-07-25 21:32:16 +0200

    changed licence comments

--- creational/singleton/singleton_multiinstance.cpp
+++ creational/singleton/singleton_multiinstance.cpp
@@ -1,17 +1,17 @@
-//This file is part of Thread-safe-design-patterns.
+//This file is part of Design-patterns.
 //
-//    Thread-safe-design-patterns is free software: you can redistribute it and/or modify
+//    Design-patterns is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU Lesser General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    Thread-safe-design-patterns is distributed in the hope that it will be useful,
+//    Design-patterns is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU Lesser General Public License for more details.
 //
 //    You should have received a copy of the GNU Lesser General Public License
-//    along with Thread-safe-design-patterns.  If not, see <http://www.gnu.org/licenses/>.
+//    along with Design-patterns.  If not, see <http://www.gnu.org/licenses/>.
 
 // thread safe singleton implementation
 // only C++14 +  (template varargs)
